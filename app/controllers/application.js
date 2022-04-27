2cd964a336477e73262bb95b247f3f85b2bc5c4b
Chris Krycho <hello@chriskrycho.com>
2022-04-27 09:52:39 -0600

    Show interop of old and new

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
+import { TrackedArray } from 'tracked-built-ins';
 
-export default class ApplicationController extends Controller {
+class Untracked {
   items = [1, 2, 3];
 
   get first() {
@@ -18,4 +19,29 @@ export default class ApplicationController extends Controller {
   push = () => {
     this.items.push(this.items.length + 1);
   };
+}
+
+class Tracked {
+  items = new TrackedArray([1, 2, 3]);
+
+  get first() {
+    return this.items.at(0);
+  }
+
+  get last() {
+    return this.items.at(-1);
+  }
+
+  oldPush = () => {
+    this.items.pushObject(this.items.length + 1);
+  };
+
+  push = () => {
+    this.items.push(this.items.length + 1);
+  };
+}
+
+export default class ApplicationController extends Controller {
+  untracked = new Untracked();
+  tracked = new Tracked();
 }
